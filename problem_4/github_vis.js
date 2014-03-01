c8d4fedb1c3a9fc4830466c8733864f51e7a4ab1
Patrick Sinco <psinco@gmail.com>
2014-03-01 14:07:46 -0600

    switches between contributor views: commits, changes

--- problem_4/github_vis.js
+++ problem_4/github_vis.js
@@ -63,7 +63,8 @@ d3.json('../misc/guides-commits-master.json', function(jsonMaster)
               'login': d.author.login, 
               'commits': d.total,
               'additions': additions,
-              'deletions': deletions
+              'deletions': deletions,
+              'changes': additions + deletions
             }
           );
         }); // end dataContrib.forEach()
@@ -72,6 +73,10 @@ d3.json('../misc/guides-commits-master.json', function(jsonMaster)
         /*
          *D3 MAGIC
          */
+
+        var node;
+        var radius;
+
         var maxCommits = d3.max(contributors, function(d) {
           return d.commits;
         });
@@ -80,57 +85,128 @@ d3.json('../misc/guides-commits-master.json', function(jsonMaster)
           return d.commits;
         });
 
+        var maxChanges = d3.max(contributors, function(d) {
+          return d.changes;
+        });
+
+        var minChanges = d3.min(contributors, function(d) {
+          return d.changes;
+        });
+
         var colorScale = d3.scale.linear()
-          .domain([minCommits, maxCommits])
           .interpolate(d3.interpolateRgb)
           .range(['blue', 'darkblue'])
 
+        // make a force layout
         var force = d3.layout.force()
-          .nodes(contributors)
           .size([width, height])
-          //.on('tick', tick)
-          .charge(function(d) {
-            // charge equation from:
-            // http://vallandingham.me/bubble_charts_in_d3.html
-            return -Math.pow(d.commits * 4 + 50, 2.0) / 8;
-          })
-          .start();
+          .on('tick', tick)
+          //.start();
 
-        var node = svg.selectAll('.node')
-          .data(contributors)
-          .enter()
-            .append('g')
-            .attr('class', 'node')
-
-        node.append('circle')
-          .attr('r', function(d) {
-            return (d.commits * 4) + 40;
-          })
-          .attr('fill', function(d) {
-            return colorScale(d.commits);
-          })
-
-        node.append('text')
-          .text(function(d) {
-            return d.login 
-          })
-          .attr('text-anchor', 'middle')
-          .attr('fill', '#ffffff')
-          .attr('font-size', '10px')
-          .attr('font-family', 'sans-serif')
+        function tick() {
+          graphUpdate(0);
+        }
 
 
-        //var tick = function(d) {
-        force.on('tick', function() {
-          svg.selectAll('.node')
-            .select('circle')
-            .attr('cx', function(d) {
-              return d.x;
-            })
-            .attr('cy', function(d) {
-              return d.y;
+        function contribViewByCommits() {
+          // update colorScale
+          colorScale
+            .domain([minCommits, maxCommits])
+
+          force
+            .nodes(contributors)
+            .charge(function(d) {
+              // set radius for this view
+              radius = d.commits * 4 + 40;
+
+              // charge equation from:
+              // http://vallandingham.me/bubble_charts_in_d3.html
+              return -Math.pow(radius, 2.0) / 8;
             })
 
+          d3.selectAll('circle')
+            .transition()
+            .duration(500)
+              .style('fill', function(d) {
+                return colorScale(d.commits); 
+              })
+            .attr('r', function(d) {
+              return (d.commits * 4) + 40;
+            })
+            .attr('fill', function(d) {
+              return colorScale(d.changes);
+            })
+
+          force
+            .start();
+        }  
+
+        function contribViewByChanges() {
+          // update colorScale
+          colorScale
+            .domain([minChanges, maxChanges])
+          
+          force
+            .nodes(contributors)
+            .charge(function(d) {
+              // set radius for this view
+              radius = d.changes + 50;
+
+              // charge equation from:
+              // http://vallandingham.me/bubble_charts_in_d3.html
+              return -Math.pow(radius, 2.0) / 8;
+            })
+
+
+          d3.selectAll('circle')
+            .transition()
+            .duration(500)
+              .style('fill', function(d) {
+                return colorScale(d.changes); 
+              })
+            .attr('r', function(d) {
+              return d.changes + 50;
+            })
+            .attr('fill', function(d) {
+              return colorScale(d.changes);
+            })
+        
+          force
+            .start();
+        }
+
+//        force.on('tick', function() {
+//          svg.selectAll('.node')
+//            .select('circle')
+//            .attr('cx', function(d) {
+//              return d.x;
+//            })
+//            .attr('cy', function(d) {
+//              return d.y;
+//            })
+
+          node = svg.selectAll('.node')
+            .data(contributors)
+            .enter()
+              .append('g')
+              .attr('class', 'node')
+
+          node.append('circle')
+            .attr('r', function(d) {
+              return (d.commits * 4) + 40;
+            })
+            .attr('fill', function(d) {
+              return colorScale(d.commits);
+            })
+
+          node.append('text')
+            .text(function(d) {
+              return d.login 
+            })
+            .attr('text-anchor', 'middle')
+            .attr('fill', '#ffffff')
+            .attr('font-size', '10px')
+            .attr('font-family', 'sans-serif')
           svg.selectAll('.node')
             .select('text')
             .attr('x', function(d) {
@@ -140,27 +216,35 @@ d3.json('../misc/guides-commits-master.json', function(jsonMaster)
               return d.y;
             })
         
-        });
+        //});
             
-//        function graphUpdate(delay) {
-//          nodes
-//            .transition()
-//            .duration(delay)
-//              .attr('transform', function(d) {
-//                //return 'translate(' + d.x + ', ' + d.y + ')';
-//                //console.log(d.x, d.y);
-//              });
-//        }
+        function graphUpdate(delay) {
+          node
+            .transition()
+            .duration(delay)
+              .attr('transform', function(d) {
+                return 'translate(' + d.x + ', ' + d.y + ')';
+                //console.log(d.x, d.y);
+              });
+        }
           
-        //force.on('tick', function(d) {
-          //console.log(d.x);
-        //}
-
           
 
-        //var forceLayout = function() {
-          
-        //}
+        /*
+         *EVENT LISTENERS
+         */
+
+        d3.select('input[value=\"commits\"]')
+          .on('click', contribViewByCommits)
+
+        d3.select('input[value=\"changes\"]')
+          .on('click', contribViewByChanges)
+
+
+        /*
+         *GET THINGS GOING
+         */
+        contribViewByCommits();
 
       }); // end dataContrib
     }); // end jsonSis
